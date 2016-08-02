a8e31af8cfba174faef4652e689470a3fe292def
Yuri Sorochkin <yuri.sorochkin@lge.com>
2016-08-02 15:37:41 +0300

    [ozone/external_wayland] Applying refactored and adapted ozone-wayland patches
    1. Applied ozone-wayland patches from upstream external ozone-wayland
       (refer to ozone-wayland repo)
        0001-Browser-Support-Desktop-Aura-creation-on-Ozone.patch
        0002-Adapt-DesktopFactoryOzone-to-respect-ozone-platform-.patch
        0003-Add-support-to-provide-external-ozone-files-in-views.patch
        0006-Fix-crash-when-switching-to-console-VT-mode.patch
        0007-Add-needed-support-in-PlatformWindow.patch
        0010-Remove-ATK-dependency.patch
        0012-Use-InputMethodAuraLinux-for-Ozone-implementations-o.patch
        0013-Add-drag-and-drop-interfaces-to-PlatformWindowDelega.patch
    2. The patches were refactored for compiling with any ozone platform
    3. The patches were adapted to chromium v53
    4. Remove X11 dependencies for build with enabled ozone
    5. Add fullscreen_ozone.cc window_icon_util_ozone.cc window_finder_ozone.cc
    6. Cleared issue with dependencies for extensions
    7. Added GN support.
    8. Adaptation to m61
    9. Added comments to changes related to ozone-wayland port
    10. Ozone GPU test helper gpu_task_runner_ renamed to gpu_io_task_runner_.

--- chrome/browser/ui/views/frame/browser_frame.cc
+++ chrome/browser/ui/views/frame/browser_frame.cc
@@ -207,7 +207,8 @@ void BrowserFrame::SchedulePaintInRect(const gfx::Rect& rect) {
 
 void BrowserFrame::OnNativeWidgetWorkspaceChanged() {
   chrome::SaveWindowWorkspace(browser_view_->browser(), GetWorkspace());
-#if defined(USE_X11)
+// Added defined(USE_X11) for compiling chrome browser with ozone-wayland port
+#if !defined(OS_CHROMEOS) && defined(USE_X11)
   BrowserList::MoveBrowsersInWorkspaceToFront(
       views::X11DesktopHandler::get()->GetWorkspace());
 #endif
