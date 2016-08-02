e7fd232b742415782030c59af05c9bc16bf2c208
Yuri Sorochkin <yuri.sorochkin@lge.com>
2016-08-02 15:42:18 +0300

    [ozone/external_wayland] Applying refactored and adapted ozone-wayland patches
    1. Applied ozone-wayland patches from upstream external ozone-wayland
       (refer to ozone-wayland repo)
        0001-Browser-Support-Desktop-Aura-creation-on-Ozone.patch
        0002-Adapt-DesktopFactoryOzone-to-respect-ozone-platform-.patch
        0003-Add-support-to-provide-external-ozone-files-in-views.patch
        0006-Fix-crash-when-switching-to-console-VT-mode.patch
        0007-Add-needed-support-in-PlatformWindow.patch
        0010-Remove-ATK-dependency.patch
        0012-Use-InputMethodAuraLinux-for-Ozone-implementations-o.patch
        0013-Add-drag-and-drop-interfaces-to-PlatformWindowDelega.patch
    2. The patches were refactored for compiling with any ozone platform
    3. The patches were adapted to chromium v53

--- ui/views/widget/desktop_aura/desktop_factory_ozone.cc
+++ ui/views/widget/desktop_aura/desktop_factory_ozone.cc
@@ -2,28 +2,37 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
 #include "ui/views/widget/desktop_aura/desktop_factory_ozone.h"
 
 #include "base/logging.h"
+#include "ui/ozone/platform_object.h"
 
 namespace views {
 
 // static
-DesktopFactoryOzone* DesktopFactoryOzone::impl_ = NULL;
+DesktopFactoryOzone* DesktopFactoryOzone::impl_ = nullptr;
 
 DesktopFactoryOzone::DesktopFactoryOzone() {
+  DCHECK(!impl_) << "There should only be a single DesktopFactoryOzone.";
+  impl_ = this;
 }
 
 DesktopFactoryOzone::~DesktopFactoryOzone() {
+  DCHECK_EQ(impl_, this);
+  impl_ = nullptr;
 }
 
 DesktopFactoryOzone* DesktopFactoryOzone::GetInstance() {
-  CHECK(impl_) << "DesktopFactoryOzone accessed before constructed";
-  return impl_;
-}
+  if (!impl_) {
+    std::unique_ptr<DesktopFactoryOzone> factory =
+        ui::PlatformObject<DesktopFactoryOzone>::Create();
 
-void DesktopFactoryOzone::SetInstance(DesktopFactoryOzone* impl) {
-  impl_ = impl;
+    // TODO(tonikitoo): Currently need to leak this object.
+    DesktopFactoryOzone* leaky = factory.release();
+    DCHECK_EQ(impl_, leaky);
+  }
+  return impl_;
 }
 
 } // namespace views
