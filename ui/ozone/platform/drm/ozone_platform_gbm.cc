2b644ef6ab37b507c312cc792d938dcbf2f0a375
Yuri Sorochkin <yuri.sorochkin@lge.com>
2017-02-02 16:21:43 +0300

    [ozone/external_wayland] Revert "ozone: Remove GpuPlatformSupport."
    Revert "ozone: Remove GpuPlatformSupport."
    ozone-wayland external port requares GpuPlatformSupport
    interface. We handle the notification when the gpu is initialized.
    So we should revert removing of the interface.
    
      ozone: Remove GpuPlatformSupport.
    
      None of the GpuPlatformSupport implementations do anything with the
      ipc messages they receive, or do anything when the gpu is initialized.
      The only functionality GpuPlatformSupport provides is on the gbm
      platform, where it installs an IPC message filter. So remove the
      GpuPlatformSupport interface, and just do that from the OzonePlatform
      interface instead.
    
      BUG=none
      R=halliwell@chromium.org, kbr@chromium.org, spang@chromium.org
    
      Review URL: https://codereview.chromium.org/2286193002 .
    
      Cr-Commit-Position: refs/heads/master@{#415042}

--- ui/ozone/platform/drm/ozone_platform_gbm.cc
+++ ui/ozone/platform/drm/ozone_platform_gbm.cc
@@ -30,6 +30,7 @@
 #include "ui/ozone/platform/drm/gpu/drm_device_generator.h"
 #include "ui/ozone/platform/drm/gpu/drm_device_manager.h"
 #include "ui/ozone/platform/drm/gpu/drm_gpu_display_manager.h"
+#include "ui/ozone/platform/drm/gpu/drm_gpu_platform_support.h"
 #include "ui/ozone/platform/drm/gpu/drm_thread_message_proxy.h"
 #include "ui/ozone/platform/drm/gpu/drm_thread_proxy.h"
 #include "ui/ozone/platform/drm/gpu/gbm_surface_factory.h"
@@ -45,6 +46,7 @@
 #include "ui/ozone/platform/drm/host/drm_window_host_manager.h"
 #include "ui/ozone/platform/drm/mus_thread_proxy.h"
 #include "ui/ozone/public/cursor_factory_ozone.h"
+#include "ui/ozone/public/gpu_platform_support.h"
 #include "ui/ozone/public/gpu_platform_support_host.h"
 #include "ui/ozone/public/ozone_platform.h"
 #include "ui/ozone/public/ozone_switches.h"
@@ -98,8 +100,8 @@ class OzonePlatformGbm : public OzonePlatform {
   InputController* GetInputController() override {
     return event_factory_ozone_->input_controller();
   }
-  IPC::MessageFilter* GetGpuMessageFilter() override {
-    return gpu_message_filter_.get();
+  GpuPlatformSupport* GetGpuPlatformSupport() override {
+    return gpu_platform_support_.get();
   }
   GpuPlatformSupportHost* GetGpuPlatformSupportHost() override {
     return gpu_platform_support_host_.get();
@@ -219,7 +221,7 @@ class OzonePlatformGbm : public OzonePlatform {
       scoped_refptr<DrmThreadMessageProxy> message_proxy(
           new DrmThreadMessageProxy());
       itmp = message_proxy.get();
-      gpu_message_filter_ = std::move(message_proxy);
+      gpu_platform_support_.reset(new DrmGpuPlatformSupport(message_proxy));
     }
 
     // NOTE: Can't start the thread here since this is called before sandbox
@@ -247,10 +249,10 @@ class OzonePlatformGbm : public OzonePlatform {
   std::unique_ptr<DrmThreadProxy> drm_thread_proxy_;
   std::unique_ptr<GlApiLoader> gl_api_loader_;
   std::unique_ptr<GbmSurfaceFactory> surface_factory_;
-  scoped_refptr<IPC::MessageFilter> gpu_message_filter_;
   // TODO(sad): Once the mus gpu process split happens, this can go away.
   std::vector<ozone::mojom::DeviceCursorRequest> pending_cursor_requests_;
   scoped_refptr<base::SingleThreadTaskRunner> gpu_task_runner_;
+  std::unique_ptr<DrmGpuPlatformSupport> gpu_platform_support_;
 
   // Objects in the Browser process.
   std::unique_ptr<DeviceManager> device_manager_;
